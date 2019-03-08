a6e5fa8cd0eb724c368d4c51312026b07ca47e20
unknown <33537@ETECAF.LOCAL>
2019-03-08 17:20:58 -0300

    Commit inicial

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+
+    public int a = 1;
+    public int b = 1;
+    public int c = 1;
+
+    void Start()
+    {
+        BaskharaL1(a, b, c);
+    }
+    #region treino
+    private void Soma(int x, int y)
+    {
+        Debug.Log(x + y);
+    }
+    private int Soma3(int x, int y, int z)
+    {
+        return x + y + z;
+    }
+    private void Media(int x, int y, int z)
+    {
+        int result = Soma3(x , y, z);
+        Debug.Log((result) / 3);
+    }
+    #endregion
+
+    #region exercicios
+    private int Delta(int a, int b, int c)
+    {
+        return (b * b) - 4 * a * c;
+    }
+    private void BaskharaL1(int a, int b, int c)
+    {
+        int delta = Delta(a, b, c);
+        Debug.Log((-b - Mathf.Sqrt(delta))/ 2 * a);
+        Debug.Log((-b + Mathf.Sqrt(delta)) / 2 * a);
+    }
+    #endregion
+}
